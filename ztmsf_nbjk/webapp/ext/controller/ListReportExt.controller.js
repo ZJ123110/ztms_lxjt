7674ee1b1583b46e0f647a64c5d5b94472a1858e
13097161303 <513929207@qq.com>
2024-07-31 01:35:54 +0800

    1

---
+++ ztmsf_nbjk/webapp/ext/controller/ListReportExt.controller.js
@@ -0,0 +1,18 @@
+sap.ui.define([
+    "sap/m/MessageToast"
+], function(MessageToast) {
+    'use strict';
+
+    return {
+        onSqidPress: function(oEvent) {
+            var oClickedRowData = oEvent.getSource().getBindingContext().getObject();
+            let url = window.location.origin + "/home/index.html?saml2=disabled#ZTMSF_LXJT_NB-manage&//ZTMS_C_LXJT(db_key=guid'" + oClickedRowData.parent_key + "',IsActiveEntity=true)";
+            window.open(url);
+        },
+        onHtdjPress: function(oEvent) {
+            // var oClickedRowData = oEvent.getSource().getBindingContext().getObject();
+            // let url = window.location.origin + "/home/index.html?saml2=disabled#ZTMSF_NBJK_HTDJ-manage&//ZTMS_C_JHTDJ(db_key=guid'" + oClickedRowData.ht_key + "',IsActiveEntity=true)";
+            // window.open(url);
+        }
+    };
+});
